fcb54993415da5bd21aea4895c65209d6a029573
Jason Rogers <jason.rogers@keytree.co.uk>
2019-04-14 17:27:44 +0100

    adding Booklist test to git

--- src/components/__test__/BookList.test.js
+++ src/components/__test__/BookList.test.js
@@ -1,33 +1,12 @@
 import React from "react";
-// import renderer from "react-test-renderer";
+import renderer from "react-test-renderer";
 import { BookList } from "../BookList";
-// import { BookService } from '../../services/bookService'
+import { Book } from "./testData";
 
 test("Book List", () => {
-  // const books = BookService.processDueDates([
-  //   {
-  //     author: 'Samuel Beckett',
-  //     country: 'Republic of Ireland',
-  //     imageLink: 'images/molloy-malone-dies-the-unnamable.jpg',
-  //     language: 'French, English',
-  //     link: 'https://en.wikipedia.org/wiki/Molloy_(novel)\n',
-  //     pages: 256,
-  //     title: 'Molloy, Malone Dies, The Unnamable, the trilogy',
-  //     year: 1952,
-  //     dueDate: '2000-03-09',
-  //   },
-  //   {
-  //     author: 'Hans Christian Andersen',
-  //     country: 'Denmark',
-  //     imageLink: 'images/fairy-tales.jpg',
-  //     language: 'Danish',
-  //     link:
-  //       'https://en.wikipedia.org/wiki/Fairy_Tales_Told_for_Children._First_Collection.\n',
-  //     pages: 784,
-  //     title: 'Fairy tales',
-  //     year: 1836,
-  //   },
-  // ])
-  // const component = renderer.create(<BookList books={books} />)
-  // expect(component.root.findAllByProps({ className: 'book-thumbnail' }).length).toBe(2)
+  const books = [Object.assign({}, Book)];
+  const component = renderer.create(<BookList books={books} />);
+  expect(
+    component.root.findAllByProps({ className: "book-thumbnail" }).length
+  ).toBe(1);
 });
